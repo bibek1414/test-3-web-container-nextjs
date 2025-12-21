4919a3b82561e1d3d994c6f8e9d61a70207210c3
bibek1414 <bbhattarai770@gmail.com>
2025-12-21 12:39:34 +0545

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="p-8 font-sans">
       <div className="mt-4">
-        <Link href="/builder/ip-store" className="text-blue-700 underline">
-          Go to Builder ip-store
+        <Link href="/builder/nextjs-web-container" className="text-blue-700 underline">
+          Go to Builder nextjs-web-container
         </Link>
       </div>
     </div>
